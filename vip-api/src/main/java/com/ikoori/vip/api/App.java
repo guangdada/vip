321e18a4becfb528a235556cebcdddf2a78bee0d
chengxiaoguang <240184295@qq.com>
2017-07-26 23:30:02 +0800

    提交parent

---
+++ vip-api/src/main/java/com/ikoori/vip/api/App.java
@@ -0,0 +1,13 @@
+package com.ikoori.vip.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ vip-common/src/main/java/com/ikoori/vip/common/App.java
@@ -0,0 +1,13 @@
+package com.ikoori.vip.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
