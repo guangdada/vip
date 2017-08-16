fb7d2bb371c7f0bde80223f1ea5b20d4e9607076
huanglin1234 <924106963@qq.com>
2017-08-16 15:13:57 +0800

    小票管理

--- vip-server/src/main/webapp/static/modular/biz/ticket/ticket.js
+++ vip-server/src/main/webapp/static/modular/biz/ticket/ticket.js
@@ -90,11 +90,37 @@ Ticket.search = function () {
 };
 
 $(function () {
+	$("#error1").css('display','none'); 
+	$("#error2").css('display','none'); 
 	 $("#title").keyup(function(){
+			var title = $("#title").val();
 		    $("#ptitle").text($("#title").val());
+		    var title_len = $("#title").val().length;
+		    if(title){
+				$("#error1").css('display','none'); 
+			}
+		    if(!title){
+				$("#error1").css('display','block'); 
+			}
+		    if(title_len<0 || title_len>10){
+		    	$("#error1 p").text("头部标题长度必须在1到10之间！");
+		    	$("#error1").css('display','block'); 
+		    }
 		  });
 	 $("#remark").keyup(function(){
+			var remark = $("#remark").val();
 		    $("#premark").text($("#remark").val());
+		    var remark_len = $("#remark").val().length;
+		    if(!remark){
+				$("#error2").css('display','block');
+			}
+		    if(remark){
+				$("#error2").css('display','none');
+			}
+		    if(remark_len<0 || remark_len>20){
+		    	$("#error2 p").text("底部备注长度必须在1到20之间");
+		    	$("#error2").css('display','block'); 
+		    }
 		  });
 	 $('input:radio[name="specType"]').change( function() {
 		 var specType = $("input[name='specType']:checked").val();
@@ -105,8 +131,12 @@ $(function () {
 			$("#ticketWidth").addClass("small");
 			}
 	 });
-    var defaultColunms = Ticket.initColumn();
+	/*$('#ensure').click(function(){
+		 alert("cc");
+		 TicketInfoDlg.editSubmit();
+	 });*/
+    /*var defaultColunms = Ticket.initColumn();
     var table = new BSTable(Ticket.id, "/ticket/list", defaultColunms);
     table.setPaginationType("server");
-    Ticket.table = table.init();
+    Ticket.table = table.init();*/
 });
