bc1c48cbe75c9bfecdcf4563cd3192463e7b10b8
chengxiaoguang <240184295@qq.com>
2017-09-04 10:00:07 +0800

    修改图片资源映射

--- vip-server/src/main/java/com/ikoori/vip/GunsApplication.java
+++ vip-server/src/main/java/com/ikoori/vip/GunsApplication.java
@@ -34,7 +34,7 @@ public class GunsApplication extends WebMvcConfigurerAdapter{
             registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
             registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
             // 图片上传目录映射
-            registry.addResourceHandler("/upload/**").addResourceLocations("file:///" + gunsProperties.getFileUploadPath() + "/");  
+            registry.addResourceHandler("/files/**").addResourceLocations("file:///" + gunsProperties.getFileUploadPath() + "/");  
         }
     }
 
